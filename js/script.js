81ca648fc9cd459daee9821cf8d8963acffe6741
Christian Parker <christianparker512@gmail.com>
2021-06-16 17:11:04 -0500

    let and const never use var

--- js/script.js
+++ js/script.js
@@ -1,2 +1,31 @@
-let js = "FrontEnd";
+let js = "FrontEnd Software Developer";
 if (js==='FrontEnd') alert ("He's a great Front End Software Engineer!")
+
+console.log("Christian");
+console.log(37);
+
+//declaring a variable
+let firstName = "Christian";
+let lastName = "Parker";
+
+//variable name conventions
+console.log(firstName);
+console.log(firstName + " " + lastName);
+
+let christan_parker = "CP";
+let $function = 50;
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof 37);
+console.log(typeof "Christian");
+
+
+let age = 50;
+console.log(age);
+age= 51;
+console.log(age);
+
+const birthYear= 1970;
+console.log(birthYear)
