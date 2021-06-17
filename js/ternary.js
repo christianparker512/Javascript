5bbb17898fd84407a8d7c7bdff9af0e9257722f8
Christian Parker <christianparker512@gmail.com>
2021-06-17 14:50:40 -0500

    ternary review

---
+++ js/ternary.js
@@ -0,0 +1,14 @@
+const age = 13;
+age >= 18 ? console.log('I like to drink wine') :
+  console.log('I like to drink water');
+
+const drink = age>=18 ? 'wine' :'water';
+console.log(drink);
+
+let drink2;
+if(age>=21){
+  drink2 = 'wine';
+} else {
+  drink2 = 'water';
+}
+console.log(drink2);
