604ee50ec0ff9be191ae6f799418da536addfeee
Christian Parker <christianparker512@gmail.com>
2021-06-17 11:41:37 -0500

    if else in the challenge review

--- js/challenge1.js
+++ js/challenge1.js
@@ -1,12 +1,20 @@
 // const BMI = mass/height ** 2;
 
-let markMass = 78;
-let markHeight = 1.69;
-let johnMass = 92;
-let johnHeight = 1.88;
+let markMass = 95;
+let markHeight = 1.88;
+let johnMass = 85;
+let johnHeight = 1.76;
 
 const BMIMark = markMass /markHeight **2;
 const BMIJohn = johnMass/johnHeight **2;
 
 console.log(BMIMark);
 console.log(BMIJohn);
+
+
+// Challenge #2
+if (BMIMark > BMIJohn){
+  console.log("Mark's BMI is greater than John's")
+} else {
+  console.log("John's BMI is higher than Mark's")
+}
