c6c9f3cb8c7baaa8d1b0ff2984430a8dc1bbf3ff
Christian Parker <christianparker512@gmail.com>
2021-06-18 11:40:22 -0500

    mutating array review

--- js/arrays.js
+++ js/arrays.js
@@ -9,3 +9,12 @@ const years = new Array(1965, 1970, 2001)
 console.log(years);
 
 console.log(parker[0]);
+console.log(parker[1]);
+console.log(parker[2]);
+
+// finding the last name, the minus one is due to the zero starting position
+console.log(parker[parker.length -1]);
+
+//mutating the array
+parker[1] ="lisa jo"
+console.log(parker);
