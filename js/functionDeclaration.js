6543341b9b81053f61d1290b1e7303fc859b2e34
Christian Parker <christianparker512@gmail.com>
2021-06-18 09:56:32 -0500

    function expression review

--- js/functionDeclaration.js
+++ js/functionDeclaration.js
@@ -1,10 +1,17 @@
 'use strict';
 
 
-
+//function declaration
 function calcAge1(birthYear){
   const age = 2021 - birthYear;
   return age;
 }
 const age1 = calcAge1(1970);
 console.log(age1);
+
+//function expression
+const calcAge2 = function(birthYear){
+  return 2021-birthYear;
+}
+const age2 = calcAge2(1970);
+console.log(age1, age2);
