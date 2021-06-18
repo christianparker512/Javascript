2855005e48002e2dce8e2ab62f645f901f23dcc4
Christian Parker <christianparker512@gmail.com>
2021-06-18 15:23:28 -0500

    looping in a loop

--- js/loopBackwards.js
+++ js/loopBackwards.js
@@ -11,3 +11,12 @@ const christianArray = [
 for (let i = christianArray.length -1; i >= 0; i--){
   console.log(i, christianArray[i]);
 }
+
+for (let exercise = 1; exercise <4; exercise++) {
+  console.log(`Starting exercise ${exercise}`
+  );
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`lifting weight repetition ${rep}`)
+  }
+}
