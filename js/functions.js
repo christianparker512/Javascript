4b7cc8943bdb170551944093291ac7e3d9a233a5
Christian Parker <christianparker512@gmail.com>
2021-06-18 10:30:37 -0500

    functions calling other functions

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,36 @@
-function logger(){
-  console.log("My name is Christian");
+// function logger(){
+//   console.log("My name is Christian");
+// }
+// logger();
+// logger();
+// logger();
+//
+// function fruit(apples, oranges){
+//   console.log(apples,oranges);
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges. `;
+//   return juice;
+// }
+// const appleJuice = fruit(12,5);
+// console.log(appleJuice);
+// console.log(fruit(12,0))
+//
+// const appleOrangeJuice = fruit(5,5);
+// console.log(appleOrangeJuice);
+//
+//
+// const num =(Number('37'));
+// console.log(num);
+
+
+function cutFruitPieces(fruit){
+  return fruit*4;
 }
-logger();
-logger();
-logger();
+function foodJuicer (apples, oranges){
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
 
-function fruit(apples, oranges){
-  console.log(apples,oranges);
-  const juice = `Juice with ${apples} apples and ${oranges} oranges. `;
-  return juice;
+  const juiceyJuice = `Juice with ${applePieces} apples and ${orangePieces} oranges`;
+  return juiceyJuice;
 }
-const appleJuice = fruit(12,5);
-console.log(appleJuice);
-console.log(fruit(12,0))
+console.log(foodJuicer(2,3));
 
-const appleOrangeJuice = fruit(5,5);
-console.log(appleOrangeJuice);
-
-
-const num =(Number('37'));
-console.log(num);
