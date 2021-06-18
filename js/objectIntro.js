feddefe5728cf322f7e1e0a3c5721d1e25da2209
Christian Parker <christianparker512@gmail.com>
2021-06-18 13:27:20 -0500

    object method review

--- js/objectIntro.js
+++ js/objectIntro.js
@@ -1,19 +1,35 @@
 const christian = {
     firstName: 'Christian',
     lastName: 'Parker',
-    age: 2021 - 1970,
+    birthYear: 1970,
     job: "software developer",
     family: [
       "Lisa",
       "Abby",
       "Quincy",
       "Baxter"
-    ]
-}
-console.log(christian);
-console.log(christian.lastName)
+    ],
+    hasDriversLicense: true,
+//     calcAge: function(){
+//     console.log(this);
+//     return 2021 - this.birthYear;
+//   }
+// };
+  calcAge: function(){
+    this.age = 2021 - this.birthYear;
+    return this.age;
+  },
+  getSummary: function(){
+      return`${this.firstName} is a ${this.calcAge()} year old ${christian.job}, and he has ${this.hasDriversLicense ? 'a ' : 'no '} driver's licence.`
+  }
+};
+// console.log(christian);
+// console.log(christian.lastName)
+//
+// const interestedIn = prompt("What do you want to know about Christian? Choose between firstName, lastName, age, job, and family?");
+// console.log(christian[interestedIn]);
+//
+// console.log(`${christian.firstName} has ${christian.family.length} family members. His wife is named ${christian.family[0]}`);
 
-const interestedIn = prompt("What do you want to know about Christian? Choose between firstName, lastName, age, job, and family?");
-console.log(christian[interestedIn]);
-
-console.log(`${christian.firstName} has ${christian.family.length} family members. His wife is named ${christian.family[0]}`);
+console.log(christian.calcAge());
+console.log(christian.getSummary());
