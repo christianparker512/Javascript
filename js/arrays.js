a2d5785312e12864705b40dfbd54dee5bdbde322
Christian Parker <christianparker512@gmail.com>
2021-06-18 11:36:01 -0500

    arrays review

---
+++ js/arrays.js
@@ -0,0 +1,11 @@
+const parker1 = "christian";
+const parker2 = "lisa";
+const parker3 = "abby";
+
+const parker = ['Christian', 'Lisa', 'Abby'];
+console.log(parker);
+
+const years = new Array(1965, 1970, 2001)
+console.log(years);
+
+console.log(parker[0]);
