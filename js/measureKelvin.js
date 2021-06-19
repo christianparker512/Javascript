7de5a11fce39b854c62a94dabccbca7d5fb16204
Christian Parker <christianparker512@gmail.com>
2021-06-19 14:20:26 -0500

    measure kelvin

---
+++ js/measureKelvin.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const measureKelvin = function(){
+  const measurement = {
+    type: 'type',
+    unit: 'celsius',
+    value: Number(prompt('Degrees celsius: ')),
+  };
+  console.table(measurement);
+
+const kelvin = measurement.value + 273;
+return kelvin;
+};
+console.log(measureKlevin());
