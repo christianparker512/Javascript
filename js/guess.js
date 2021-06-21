0987b07eb54b61c6caca91736fbd4d6783edece8
Christian Parker <christianparker512@gmail.com>
2021-06-21 11:54:55 -0500

    event listeners as click adding function with code to executre when pushed

--- js/guess.js
+++ js/guess.js
@@ -1,4 +1,18 @@
 "use strict";
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+//
+//
+// document.querySelector('.message').textContent = 'Correct Number 🤘🏻';
+//
+// document.querySelector('.number').textContent = 37;
+//
+// document.querySelector('.score').textContent =20;
+//
+// document.querySelector('.guess').value = 23;
+//
+//
 
+document.querySelector('.check').addEventListener('click', function(){
+  console.log(document.querySelector('.guess').value);
+})
